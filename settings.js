9961ce3f663d611890209f66b1134645c76daa51
aldi2609 <109309282+aldi2609@users.noreply.github.com>
2022-07-16 00:02:38 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -37,9 +37,9 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['6285822347348'] //ur owner number
-global.ownername = "ArullOfc" //ur owner name
-global.ytname = "YT: ARULL OFC" //ur yt chanel name
+global.vcardowner = ['628999625005'] //ur owner number
+global.ownername = "Aldi" //ur owner name
+global.ytname = "IG: aldii_260" //ur yt chanel name
 global.socialm = "GitHub: Warikrr" //ur github or insta name
 global.location = "Indonesia, Kalimantan Selatan, Kotabaru" //ur location
 
